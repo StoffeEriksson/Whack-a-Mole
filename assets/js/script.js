c09f4cd7bc4eed506c5972dc29b4a4c6a198c7ab
StoffeEriksson <djmr0kuk@students.codeinstitute.net>
2025-04-11 12:53:16 +0200

    added some variables and eventlisteners to start and reset button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
+// Variables needed
+const startButton = document.getElementById("start-btn");
+const resetButton = document.getElementById("reset-btn");
+const displayScore = document.getElementById("score");
+const holes = document.querySelectorAll(".hole");
+
+let score = 0;
+let currentHole = null;
+let moleInterval = null;
+
+// Start the game
+startButton.addEventListener("click", startGame);
+resetButton.addEventListener("click", resetGame);
+
 function startGame() {
 
 }
@@ -10,3 +24,10 @@ function randomHole() {
 
 }
 
+function showMole() {
+
+}
+
+function wackMole() {
+
+}
